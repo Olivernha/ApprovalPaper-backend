2051fa580187fe858fd0d7ff9b8212df8516bb5d
Nay Htet Aung <nayha@tuaspower.com.sg>
2025-05-14 18:01:57 +0800

    feat: add indexing for collection

--- app/main.py
+++ app/main.py
@@ -1,13 +1,16 @@
 from fastapi import FastAPI
 
 from app.database import MongoDB
+from app.services import DocumentTypeService
 
 async def lifespan(app: FastAPI):
     """Lifespan event for the application"""
     print("Connecting to MongoDB...")
     await MongoDB.connect_to_database()
+    await DocumentTypeService.ensure_indexes()
     print("Connected to MongoDB.")
     yield
+    await MongoDB.close_database_connection()
 
 app = FastAPI(lifespan=lifespan)
 
